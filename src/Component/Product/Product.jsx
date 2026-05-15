fb1688fd33fa6090b536c9cfcfdba5b9772825ba
mazenfarht <mazenmostafa2022@gmail.com>
2026-05-16 01:34:30 +0300

    Redesign product cards with modern dark UI and load more functionality

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -6,6 +6,7 @@ import { nostify } from "../Utile/notify";
 export default function Product({ product }) {
   let { changeCart, addWishlist } = useContext(StoreContext);
   let [loadingId, setLoadinId] = useState(null);
+  let [visibleCount, setVisibleCount] = useState(8);
 
   async function addProduct(productId) {
     try {
@@ -34,57 +35,89 @@ export default function Product({ product }) {
   }
   return (
     <>
-      {product.map((item) => {
+      {product.slice(0, visibleCount).map((item) => {
         return (
-          <div className="col-md-4 col-lg-3" key={item._id}>
-            <div className="product">
-              <Link to={"/product-detailes/" + item._id}>
-                <img src={item.imageCover} className=" product-img" alt="" />
-                <h6 className="my-2 text-center">{item.category.name}</h6>
-                <p className="fw-bolder text-center">
-                  {item.title.split(" ").slice(0, 3).join(" ")}
-                </p>
-              </Link>
-              <div className="d-flex justify-content-center  align-items-center my-3">
-                <span className="me-5">{item.price} EGP</span>
-                <div>
-                  <i class="fa-solid fa-star rating-color"></i>
-                  <span>{item.ratingsAverage}</span>
+          <div className="col-md-4 col-lg-3 mb-4" key={item._id}>
+            <div className="modern-product-card h-100">
+              <Link
+                to={"/product-detailes/" + item._id}
+                className="text-decoration-none"
+              >
+                {/* IMAGE */}
+                <div className="modern-product-img-wrapper">
+                  <img
+                    src={item.imageCover}
+                    className="modern-product-img"
+                    alt=""
+                  />
                 </div>
-              </div>
 
-              <button
-                onClick={() => addProduct(item._id)}
-                className="btn bg-main  text-white w-50 my-3  "
-                disabled={loadingId === item._id}
-              >
-                {loadingId === item._id ? (
+                {/* CONTENT */}
+                <div className="p-3">
+                  <span className="modern-category">{item.category.name}</span>
+
+                  <h6 className="modern-title mt-3">
+                    {item.title.split(" ").slice(0, 3).join(" ")}
+                  </h6>
+
+                  <div className="d-flex justify-content-between align-items-center mt-4">
+                    <span className="modern-price">{item.price} EGP</span>
+
+                    <div className="modern-rating">
+                      <i className="fa-solid fa-star"></i>
+                      <span>{item.ratingsAverage}</span>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+
+              {/* BUTTONS */}
+              <div className="p-3 pt-0 d-flex flex-column gap-2">
+                <button
+                  onClick={() => addProduct(item._id)}
+                  className="modern-cart-btn"
+                  disabled={loadingId === item._id}
+                >
+                  {loadingId === item._id ? (
+                    <>
+                      <i className="fa fa-spinner fa-spin me-2"></i>
+                      Adding...
+                    </>
+                  ) : (
+                    <>
+                      <i className="fa-solid fa-cart-shopping me-2"></i>
+                      Add To Cart
+                    </>
+                  )}
+                </button>
+
+                <button
+                  onClick={() => addProductToWishList(item._id)}
+                  className="modern-wishlist-btn"
+                  disabled={loadingId === item._id}
+                >
                   <>
-                    <i className="fa fa-spinner fa-spin me-2"></i>
-                    Adding...
+                    <i className="fa-regular fa-heart me-2"></i>
+                    Wishlist
                   </>
-                ) : (
-                  "Add To Cart"
-                )}
-              </button>
-              <button
-                onClick={() => addProductToWishList(item._id)}
-                className="btn bg-main  text-white w-50 my-3  "
-                disabled={loadingId === item._id}
-              >
-                {loadingId === item._id ? (
-                  <>
-                    <i className="fa fa-spinner fa-spin me-2"></i>
-                    Adding...
-                  </>
-                ) : (
-                  "Add To WishList"
-                )}
-              </button>
+                </button>
+              </div>
             </div>
           </div>
         );
       })}
+
+      {/* LOAD MORE */}
+      <div className="text-center mt-4 mb-5">
+        {visibleCount < product.length && (
+          <button
+            onClick={() => setVisibleCount((prev) => prev + 6)}
+            className="modern-load-btn"
+          >
+            Load More
+          </button>
+        )}
+      </div>
     </>
   );
 }
